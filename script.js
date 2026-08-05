af65645e77098e58beb37928b1f85c0f17c397c1
Alanoca Hazael <hazael013@gmail.com>
2026-08-04 21:19:25 -0400

    Validaciones con js

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ const menuPrincipal = document.getElementById('menu-principal');
 btnMenu.addEventListener('click', function() {
     menuPrincipal.classList.toggle('mostrar-menu');
 });
+
+const formularioContacto = document.querySelector("#form-contacto");
+const avisoContacto = document.querySelector("#aviso-contacto");
+
+function revisarFormulario(event) {
+    event.preventDefault();
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;
+    if (nombre === "") {
+        avisoContacto.textContent = "Falta tu nombre.";
+        avisoContacto.classList.add("error");
+        avisoContacto.classList.remove("exito");
+    } else if (correo.includes("@") === false) {
+        avisoContacto.textContent = "Ese correo no parece válido, le falta el símbolo de @.";
+        avisoContacto.classList.add("error");
+        avisoContacto.classList.remove("exito");
+    } else {
+        avisoContacto.textContent = "Mensaje recibido.";
+        avisoContacto.classList.add("exito");
+        avisoContacto.classList.remove("error");
+    }
+}
+
+formularioContacto.addEventListener("submit", revisarFormulario);
